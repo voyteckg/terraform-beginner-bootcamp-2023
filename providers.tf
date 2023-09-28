ef5d35735d9f743d990053c632684b4848c5227f
voyteckg 1 <goralczyk.wojciech@gmail.com>
2023-09-28 07:50:35 +0000

    #23 change structure of root module and add tagging to bucket

---
+++ providers.tf
@@ -0,0 +1,26 @@
+terraform {
+  # cloud {
+  #   organization = "Demo-Test321"
+  #   workspaces {
+  #     name = "terra-house-1"
+  #   }
+  # }
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+provider "random" {
+  # Configuration options
+}
