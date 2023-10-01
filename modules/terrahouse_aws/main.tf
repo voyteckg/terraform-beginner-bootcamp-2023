3c5d6ad428a1e1c9872e0e817cab8d6adab81188
voyteckg <48118593+voyteckg@users.noreply.github.com>
2023-10-01 22:58:38 +0200

    #28 refactor s3 bucket into the new terrahouse_aws module (#29)

---
+++ modules/terrahouse_aws/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+}
+
+provider "aws" {
+}
+
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.bucket_name
+  # Bucket Naming rules
+  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
+  tags = {
+    UserUuid = var.user_uuid
+  }
+}
