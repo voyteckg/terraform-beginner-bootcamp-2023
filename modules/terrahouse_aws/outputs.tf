ad8eab5eebbceb0debdca2b7707517a88b2198c1
voyteckg <48118593+voyteckg@users.noreply.github.com>
2023-10-03 11:57:07 +0200

    #30 eanbled static website hosting, uploaded index and error via terraform (#31)

--- modules/terrahouse_aws/outputs.tf
+++ modules/terrahouse_aws/outputs.tf
@@ -1,3 +1,8 @@
 output "bucket_name" {
   value = aws_s3_bucket.website_bucket.bucket
 }
+
+output "website_endpoint" {
+  description = "Bucket name for our static website hosting"
+  value = aws_s3_bucket_website_configuration.web_configuration.website_endpoint
+}
