8d3d2c20c6d1d9830ca73f5378e38c90dae5e71f
voyteckg 1 <goralczyk.wojciech@gmail.com>
2023-10-12 22:03:17 +0000

    #54 test our terra home on terratowns test town

--- variables.tf
+++ variables.tf
@@ -1,19 +1,25 @@
-variable "user_uuid" {
+variable "terratowns_endpoint" {
  type = string
- }
-
- variable "bucket_name" {
- type = string
- }
-
- variable "index_html_filepath" {
-  description = "Path to the index.html"
-  type        = string
 }
 
- variable "error_html_filepath" {
-  description = "Path to the error.html"
-  type        = string
+variable "terratowns_access_token" {
+ type = string
+}
+
+variable "teacherseat_user_uuid" {
+ type = string
+}
+
+variable "bucket_name" {
+ type = string
+}
+
+variable "index_html_filepath" {
+  type = string
+}
+
+variable "error_html_filepath" {
+  type = string
 }
 
 variable "content_version" {
