edc6b91f2ad5884413a5dce9fbfddd3b80c2001c
voyteckg <48118593+voyteckg@users.noreply.github.com>
2023-10-13 00:03:56 +0200

    #54 test our terra home on terratowns test town (#55)

--- main.tf
+++ main.tf
@@ -23,30 +23,33 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid
+  token = var.terratowns_access_token
 }
 
-#module "terrahouse_aws" {
-#  source = "./modules/terrahouse_aws"
-#  user_uuid = var.user_uuid
-#  bucket_name = var.bucket_name
-#  index_html_filepath = var.index_html_filepath
-#  error_html_filepath = var.error_html_filepath
-#  content_version = var.content_version
-#}
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.teacherseat_user_uuid
+  #bucket_name = var.bucket_name
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  content_version = var.content_version
+  assets_path = var.assets_path
+}
 
 resource "terratowns_home" "home" {
-  name = "How to play Arcanum in 2023!!"
+  name = "Super Mario Odyssey"
   description = <<DESCRIPTION
-Arcanum is a game from 2001 that shipped with alot of bugs.
-Modders have removed all the originals making this game really fun
-to play (despite that old look graphics). This is my guide that will
-show you how to play arcanum without spoiling the plot.
+In the game, Mario explores various kingdoms and collects Power Moons hidden inside them,
+used as a fuel source to power an airship known as the Odyssey to travel to new locations.
+Cappy's main function is the "capture" ability, 
+letting Mario possess enemies and other objects, helping him solve puzzles and progress in the game.
+After completing the main story mode, other post-game kingdoms are unlockable as well as an additional minigame,
+"Luigi's Balloon World". The game supports multiplayer and virtual reality with a Nintendo Labo virtual reality kit.
 DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net"
-  town = "gamers-grotto"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  #domain_name = "6fdq6gz.cloudfront.net"
+  town = "missingo"
   content_version = 1
 }
--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -6,15 +6,15 @@ variable "user_uuid" {
     error_message = "The user_uuid value is not a valid UUID."
   }
 }
-variable "bucket_name" {
-  description = "Name of the AWS S3 bucket"
-  type        = string
+# variable "bucket_name" {
+#   description = "Name of the AWS S3 bucket"
+#   type        = string
 
-  validation {
-    condition     = can(regex("^[a-zA-Z0-9.-]{3,63}$", var.bucket_name))
-    error_message = "Invalid bucket name. It must be between 3 and 63 characters long and can only contain letters, numbers, hyphens, and periods."
-  }
-}
+#   validation {
+#     condition     = can(regex("^[a-zA-Z0-9.-]{3,63}$", var.bucket_name))
+#     error_message = "Invalid bucket name. It must be between 3 and 63 characters long and can only contain letters, numbers, hyphens, and periods."
+#   }
+# }
 
 variable "index_html_filepath" {
   description = "Path to the index.html"
